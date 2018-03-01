123602e15b7a9bb4b57f04db78951bfe35e3c97b
Murali Gopalakrishnan <murali80027@yahoo.com>
2018-03-01 08:34:48 -0700

    Resolved conflicts

--- src/main/java/m3/guesstimator/internal/ServiceServerResource.java
+++ src/main/java/m3/guesstimator/internal/ServiceServerResource.java
@@ -1,75 +1,47 @@
 package m3.guesstimator.internal;
 
+import m3.guesstimator.internal.data.EstimatorResponse;
+import m3.guesstimator.internal.data.EstimatorServiceDao;
+import m3.guesstimator.model.SolutionArtifact;
+
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import org.restlet.Application;
-import org.restlet.resource.Delete;
-import org.restlet.resource.Get;
-import org.restlet.resource.Put;
-import org.restlet.resource.ServerResource;
-
-import m3.guesstimator.model.functional.Service;
-import m3.guesstimator.service.ServiceResource;
-
 //This resource is mapped to path "/service/{id}"
-public class ServiceServerResource extends ServerResource implements ServiceResource {
-    EntityManager em;
+public class ServiceServerResource extends AbstractServerResource {
 
-    String serviceId;
+//  @Override
+//  @Delete("json")
+//  public void remove() {
+//  	em.remove(serviceId);
+//  }
+
+//    @Override
+//    protected void doRelease() {
+//    	if (em != null) {
+//        	GuesstimatorApplication gapp = getApp();
+//        	gapp.ctx.releaseEntityManager();
+//        	em = null;
+//    	}
+//    }
 
     @Override
-    protected void doInit() {
-    	GuesstimatorApplication gapp = getApp();
-    	em = gapp.ctx.getEntityManager();
-
-        // extract matched parameters
-    	serviceId = getAttribute("id");
+//  @Put("json")
+    public EstimatorResponse put(SolutionArtifact ent) {
+//      em.persist(value);
+        return null;
     }
 
     @Override
-	@Get("json")
-	public Service retrieve() {
-		return em.find(Service.class, serviceId);
-	}
+//  @Get("json")
+    public EstimatorResponse get(String id) {
+		// return em.find(Service.class, serviceId);
+        return null;
+    }
 
-	@Override
-	@Put("json")
-	public Service store(Service value) {
-		em.persist(value);
-		TypedQuery<Service> q = em.createQuery("from Issue where description = :desc", Service.class);
-		q.setParameter("desc", value.getDescription());
-		List<Service> result = q.getResultList();
-		if (result != null && !result.isEmpty()) {
-			return result.get(0);
+	private EstimatorServiceDao getDao() {
+		if (dao instanceof EstimatorServiceDao) {
+			return (EstimatorServiceDao) dao;
 		}
-		return null;
-	}
-
-	@Override
-	@Delete("json")
-	public void remove() {
-		em.remove(serviceId);
-	}
-
-	@Override
-	protected void doRelease() {
-		if (em != null) {
-	    	GuesstimatorApplication gapp = getApp();
-	    	gapp.ctx.releaseEntityManager();
-	    	em = null;
-		}
-	}
-
-	private GuesstimatorApplication getApp() {
-    	Application app = getApplication();
-    	if (app instanceof GuesstimatorApplication) {
-    		return (GuesstimatorApplication)app;
-    	} else {
-    		// TODO throw
-    	}
-    	return null;
+		throw new ClassCastException("Invalid dao class " + dao.getClass().getName() + ".");
 	}
 }
